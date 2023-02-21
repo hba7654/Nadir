cf4556fed77bf0de02d3a38c250c2e35a231b40a
hba7654 <54544232+hba7654@users.noreply.github.com>
2023-02-20 21:31:56 -0500

    the best zombie spawner

--- Nadir/Assets/Scripts/GameManager.cs
+++ Nadir/Assets/Scripts/GameManager.cs
@@ -5,14 +5,19 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Global varibales")]
     public static float dopamine;
-    public static ArrayList zombies;
-
-    [SerializeField] private int maxZombieCount;
-    [SerializeField] private int zombieSpawnFrequency;
     [SerializeField] private Camera camera;
     [SerializeField] private GameObject zombieObject;
     [SerializeField] private GameObject playerObject;
+
+    [Header("Zombie Spawning Variables")]
+    public static ArrayList zombies;
+    [SerializeField] private int maxZombieCount;
+    [SerializeField] private int zombieSpawnFrequency;
+    [SerializeField] private int zombiesToSpawnAtOnce;
+
+    private bool isSpawning;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +25,17 @@ public class GameManager : MonoBehaviour
         dopamine = 5;
 
         zombies = new ArrayList();
+
+        isSpawning = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(zombies.Count < maxZombieCount && Mathf.FloorToInt(Time.time) % zombieSpawnFrequency == 0)
+        if(!isSpawning && (Mathf.FloorToInt(Time.time) % (zombieSpawnFrequency * 5 / dopamine)) == 0)
         {
-            SpawnZombie();
+            isSpawning = true;
+            StartCoroutine(SpawnZombie());
         }
     }
 
@@ -39,34 +47,46 @@ public class GameManager : MonoBehaviour
         Debug.Log(dopamine);
     }
 
-    private void SpawnZombie()
+    private IEnumerator SpawnZombie()
     {
         float height = camera.orthographicSize * 2;
         float width = height * camera.aspect;
-        int spawnIndex = Random.Range(0, 4);
-        Vector2 spawnPos;
-        switch(spawnIndex)
+
+        int numSpawned = 0;
+
+        while (zombies.Count < (maxZombieCount + dopamine * 3) && numSpawned < (zombiesToSpawnAtOnce + dopamine))
         {
-            case 0:
-                spawnPos = new Vector2(camera.transform.position.x + Random.Range(-width, width), camera.transform.position.y + height);
-                break;
+            int spawnIndex = Random.Range(0, 4);
+            Vector2 spawnPos;
+            switch (spawnIndex)
+            {
+                case 0:
+                    spawnPos = new Vector2(camera.transform.position.x + Random.Range(-width, width), camera.transform.position.y + height);
+                    break;
 
-            case 1:
-                spawnPos = new Vector2(camera.transform.position.x + Random.Range(-width, width), camera.transform.position.y - height);
-                break;
+                case 1:
+                    spawnPos = new Vector2(camera.transform.position.x + Random.Range(-width, width), camera.transform.position.y - height);
+                    break;
 
-            case 2:
-                spawnPos = new Vector2(camera.transform.position.x + width, camera.transform.position.y + Random.Range(-height, height));
-                break;
+                case 2:
+                    spawnPos = new Vector2(camera.transform.position.x + width, camera.transform.position.y + Random.Range(-height, height));
+                    break;
 
-            default:
-                spawnPos = new Vector2(camera.transform.position.x - width, camera.transform.position.y + Random.Range(-height, height));
-                break;
+                default:
+                    spawnPos = new Vector2(camera.transform.position.x - width, camera.transform.position.y + Random.Range(-height, height));
+                    break;
 
+            }
+            GameObject zombie = Instantiate(zombieObject, spawnPos, Quaternion.identity);
+            zombie.GetComponent<ZombieController>().playerObject = playerObject;
+            zombies.Add(zombie);
+
+            numSpawned++;
+
+            yield return new WaitForSeconds(zombieSpawnFrequency/dopamine);
         }
-        GameObject zombie = Instantiate(zombieObject, spawnPos, Quaternion.identity);
-        zombie.GetComponent<ZombieController>().playerObject = playerObject;
-        zombies.Add(zombie);
+
+        isSpawning = false;
         
     }
 }
