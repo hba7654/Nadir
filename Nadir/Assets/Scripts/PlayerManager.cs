39166c1de2f2262b266df220acb2f0621b7debf6
hba7654 <54544232+hba7654@users.noreply.github.com>
2023-03-23 14:13:07 -0400

    Cleaned up player controller

---
+++ Nadir/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public int health;
+
+    [SerializeField] private int startHealth;
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private Tile mockTile;
+    [SerializeField] private GameObject mountainTileSet;
+
+    private int bombParts;
+
+    private Rigidbody2D rb;
+    private bool questPickup1;
+    private bool questPickup2;
+    private bool questPickup3;
+
+    private int questStep;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = startHealth;
+        rb = GetComponent<Rigidbody2D>();
+
+        questPickup1 = false;
+        questPickup2 = false;
+        questPickup3 = false;
+        bombParts = 1;
+
+        questStep = 1;
+    }
+
+    private void Update()
+    {
+        if (!GameManager.isPaused)
+        {
+            
+            if (health <= 0)
+            {
+                health = 0;
+                Debug.Log("Dead");
+                SceneManager.LoadScene("Main Menu");
+            }
+
+            if (!questPickup1 && rb.position.x > 20.5 && rb.position.x < 21.5 && rb.position.y > 24 && rb.position.y < 25)
+            {
+                questPickup1 = true;
+                tilemap.SetTile(new Vector3Int(42, 48, 0), null);
+
+                bombParts++;
+                Debug.Log("QUEST PICKUP  " + bombParts + " Bomb parts have been collected");
+            }
+
+            if (!questPickup2 && rb.position.x > -35 && rb.position.x < -34 && rb.position.y > 8.5 && rb.position.y < 9.5)
+            {
+                questPickup2 = true;
+                tilemap.SetTile(new Vector3Int(-70, 17, 0), null);
+
+                bombParts++;
+                Debug.Log("quest pickup  " + bombParts + " bomb parts have been collected");
+            }
+
+            if (bombParts == 3)
+            {
+                Debug.Log("Mountain EXPLODED!!!!");
+                mountainTileSet.SetActive(false);
+                bombParts++; 
+            }
+        }
+    }
+
+
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Zombie")
+        {
+            health--;
+            Debug.Log("OUCH, health = " + health);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "Health")
+        {
+            health += 5;
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Nadir/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject mainFloor;
+    [SerializeField] private GameObject caveOne;
+    [SerializeField] private GameObject caveTwo;
+    [SerializeField] private GameObject caveTwoInner;
+
+    private Rigidbody2D rb;
+    private bool isMoving;
+    private Vector2 moveVector;
+    private Animator animator;
+    private SpriteRenderer playerSr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //isMoving = false;
+        moveVector = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+
+        playerSr = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!GameManager.isPaused)
+        {
+            rb.velocity = moveVector * moveSpeed * GameManager.dopamine;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isMoving = true;
+            animator.SetBool("Walking", true);
+        }
+        else if (context.canceled)
+        {
+            isMoving = false;
+            animator.SetBool("Walking", false);
+        }
+
+        moveVector = context.ReadValue<Vector2>();
+
+        if (moveVector.x > 0)
+        {
+            playerSr.flipX = false;
+        }
+        else if (moveVector.x < 0)
+        {
+            playerSr.flipX = true;
+        }
+
+        // Debug.Log(rb.position.x + " , " + rb.position.y);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Cave 1")
+        {
+            mainFloor.SetActive(false);
+            caveOne.SetActive(true);
+        }
+        else if (collision.tag == "Cave 2")
+        {
+            mainFloor.SetActive(false);
+            caveTwoInner.SetActive(false);
+            caveTwo.SetActive(true);
+        }
+        else if (collision.tag == "Cave 2 Inner")
+        {
+            caveTwo.SetActive(false);
+            caveTwoInner.SetActive(true);
+        }
+        else if (collision.tag == "Main Floor")
+        {
+            mainFloor.SetActive(true);
+            caveOne.SetActive(false);
+            caveTwo.SetActive(false);
+        }
+    }
+}
