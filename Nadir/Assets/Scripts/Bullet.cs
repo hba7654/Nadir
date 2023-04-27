c69240784a48d4c07f7e72625319a3f683fbb277
hba7654 <54544232+hba7654@users.noreply.github.com>
2023-04-27 04:13:02 -0400

    GIVE LIFE BACK TO MUSIC

--- Nadir/Assets/Scripts/Bullet.cs
+++ Nadir/Assets/Scripts/Bullet.cs
@@ -6,15 +6,7 @@ public class Bullet : MonoBehaviour
 {
     [Header("Movement Variables")]
     [SerializeField] private float bulletSpeed;
-    private Vector2 currentVelocity;
     private Rigidbody2D rb;
-
-
-    [Header("Cleanup Variables")]
-    //This is for cleaning up created bullets when the pass a certain point
-    [SerializeField] private float maxDistanceX;
-    [SerializeField] private float maxDistanceY;
-    private Vector2 initialPos;
 
 
 
@@ -23,19 +15,13 @@ public class Bullet : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    public void Update()
+    public void OnBecameInvisible()
     {
-        //checks position, if out of bounds destroy the object
-        if (transform.position.x > initialPos.x + maxDistanceX || transform.position.x < initialPos.x - maxDistanceX
-            || transform.position.y > initialPos.y + maxDistanceY || transform.position.y < initialPos.y - maxDistanceY)
-        {
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 
     public void InitialMove(Vector2 initalVelocity)
     {
-        initialPos = transform.position;
         rb.velocity = initalVelocity * bulletSpeed * GameManager.dopamine;
     }
 
--- Nadir/Assets/Scripts/SoundManager.cs
+++ Nadir/Assets/Scripts/SoundManager.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioClip[] soundClips;
+    [SerializeField] private AudioClip[] songs;
+    [SerializeField] private Slider sfxVolumeSlider;
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private AudioSource musicPlayer;
 
     private static AudioSource audioSource;
+    private static Slider sfxVolumeSliderStatic;
     private static AudioClip[] soundClipsStatic;
     // Start is called before the first frame update
 
     private void Start()
     {
         soundClipsStatic = soundClips;
+        sfxVolumeSliderStatic = sfxVolumeSlider;
         audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        musicPlayer.volume = GameManager.isPaused ? musicVolumeSlider.value * 0.5f : musicVolumeSlider.value;
+        if(!musicPlayer.isPlaying)
+        {
+            musicPlayer.clip = songs[Random.Range(0, songs.Length)];
+            musicPlayer.Play();
+        }
     }
 
     public static void PlaySound(string sound)
@@ -25,8 +42,8 @@ public class SoundManager : MonoBehaviour
             return;
         }
 
-        if(!audioSource.isPlaying)
-            audioSource.PlayOneShot(clip);
+        //if(!audioSource.isPlaying)
+            audioSource.PlayOneShot(clip, sfxVolumeSliderStatic.value);
     }
 
     private static AudioClip Contains(string name)
@@ -37,6 +54,13 @@ public class SoundManager : MonoBehaviour
                 return soundClipsStatic[i];
         }
         return null;
+    }
+
+    public void NextSong()
+    {
+        musicPlayer.Stop();
+        musicPlayer.clip = songs[Random.Range(0, songs.Length)];
+        musicPlayer.Play();
     }
 
 }
